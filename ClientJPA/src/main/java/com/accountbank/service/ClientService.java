adbecc13c162009b0ff26e2b52f9a87dab51eafc
Josias Luis Quiñonez Ramos <jquinonr@emeal.nttdata.com>
2022-08-24 15:44:16 -0500

    ruc_dni tipo Long

--- ClientJPA/src/main/java/com/accountbank/service/ClientService.java
+++ ClientJPA/src/main/java/com/accountbank/service/ClientService.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 
 public interface ClientService {
     List<Client> getClients();
-    List<Client> getById(Integer ruc_dni);
+    List<Client> getById(Long ruc_dni);
     List<Client> create(Client client);
     List<Client> update(Client client);
-    void delete(Integer ruc_dni);
+    void delete(Long ruc_dni);
 }
